534adfc4c119ae8210b1a0b31700dd7b9e046e19
Hyunsoo_kim <red_panda119@naver.com>
2026-06-08 04:51:37 +0900

    refactor : 미들웨어, 컨트롤러 분리

--- src/controller/post/checkPasswordController.ts
+++ src/controller/post/checkPasswordController.ts
@@ -1,7 +1,11 @@
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express';
 
 const checkPasswordController = async (req: Request, res: Response): Promise<any> => {
   const { token } = res.locals;
+  
+  if (!token) {
+    return res.status(400).json({ message: '인증 토큰이 생성되지 않았습니다.' });
+  }
   
   return res.status(200).json({ token });
 };
