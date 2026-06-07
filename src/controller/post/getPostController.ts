315c800d46a941e2c579bbe3782614243ecb10c8
Hyunsoo_kim <red_panda119@naver.com>
2026-06-08 04:59:13 +0900

    style : 코드 포맷팅

--- src/controller/post/getPostController.ts
+++ src/controller/post/getPostController.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express';
 
 const getPostController = async (req: Request, res: Response): Promise<any> => {
   const { post } = res.locals;
-  
+
   return res.status(200).json(post);
 };
 
