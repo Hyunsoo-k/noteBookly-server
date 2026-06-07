c7ead39f14b970aea62639979bc9c9fd5c113f68
Hyunsoo_kim <red_panda119@naver.com>
2026-06-08 04:52:59 +0900

    refactor : 미들웨어, 컨트롤러 분리

--- src/controller/post/createPostController.ts
+++ src/controller/post/createPostController.ts
@@ -1,9 +1,25 @@
-import type { Request, Response } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 
-const createPostController = async (req: Request, res: Response): Promise<any> => {
-  const { createdPost } = res.locals;
+import PostModel from '../../model/post.js';
 
-  return res.status(201).json(createdPost)
+const createPostController = async (
+  req: Request, 
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const { sanitizedBody } = res.locals;
+
+    if (!sanitizedBody) {
+      return res.status(500).json({ message: '등록할 데이터가 유실되었습니다.' });
+    }
+
+    const createdPost = await PostModel.create(sanitizedBody);
+
+    return res.status(201).json(createdPost);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default createPostController;
