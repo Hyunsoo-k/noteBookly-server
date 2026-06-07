642f557a8934675834707733cf5f04bfad78945a
Hyunsoo_kim <red_panda119@naver.com>
2026-06-08 04:49:53 +0900

    refactor/미들웨어, 컨트롤러 분리

--- src/controller/posts/getPostsController.ts
+++ src/controller/posts/getPostsController.ts
@@ -1,9 +1,38 @@
-import { Request, Response } from 'express';
+import type { Request, Response } from "express";
+import type { QueryOptions } from "mongoose";
 
-const getPostsController = async (req: Request, res: Response): Promise<any> => {
-  const { posts, hasNextPage } = res.locals;
+import type { PostWithoutPassword } from "../../types/post.js";
+import PostModel from "../../model/post.js";
+import optimizePost from "../../utils/optimizePost.js";
+
+const getPostsController = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  const filter = res.locals.dbFilter || {};
+  
+  const projectionPassword: QueryOptions['projection'] = { password: 0 };
+
+  const limit = 8;
+
+  const posts: PostWithoutPassword[] = await PostModel
+    .find(filter, projectionPassword)
+    .sort({ _id: -1 })
+    .limit(limit + 1)
+    .lean();
+    
+  const hasNextPage: boolean = posts.length > limit;
+
+  if (hasNextPage) {
+    posts.pop();
+  }
+
+  const optimizedPostList = await Promise.all(
+    posts.map((post: PostWithoutPassword) => optimizePost(post))
+  );
+
   return res.status(200).json({
-    posts,
+    posts: optimizedPostList,
     hasNextPage
   });
 };
--- src/middleware/posts/getPostsMiddleware.ts
+++ src/middleware/posts/getPostsMiddleware.ts
@@ -1,29 +1,20 @@
 import type { NextFunction, Request, Response } from "express";
-import type { QueryOptions } from "mongoose";
 import { Types } from 'mongoose';
-
-import type { PostWithoutPassword } from "../../types/post.js";
-import PostModel from "../../model/post.js";
-import optimizePost from "../../utils/optimizePost.js";
 
 interface Query {
   select?: 'writer' | 'titleAndContent';
   query?: string;
   cursor?: string;
-};
+}
 
-const getPostsMiddleware = async(
+const getPostsMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<any> => {
   const { select, query, cursor }: Query = req.query;
 
-  let filter = {};
-
-  const projectionPassword: QueryOptions['projection'] = { password: 0 };
-
-  const limit = 8;
+  let filter: Record<string, any> = {};
 
   if (select || query) {
     if (select !== 'writer' && select !== 'titleAndContent') {
@@ -35,47 +26,21 @@ const getPostsMiddleware = async(
     }
 
     if (select === 'writer') {
-      filter = {
-        ...filter,
-        writer: { $regex: query, $option: 'i'}
-      }
+      filter.writer = { $regex: query, $options: 'i' }; 
     } else if (select === 'titleAndContent') {
-      filter = {
-        ...filter,
-        $or: [
-          { title: { $regex: query, $options: "i" } },
-          { content: { $regex: query, $options: "i" } }
-        ]
-      }
+      filter.$or = [
+        { title: { $regex: query, $options: "i" } },
+        { content: { $regex: query, $options: "i" } }
+      ];
     }
   }
 
   if (cursor) {
-    filter = {
-      ...filter,
-      _id: { $lt: new Types.ObjectId(String(cursor)) }
-    }
+    filter._id = { $lt: new Types.ObjectId(String(cursor)) };
   }
 
-  const posts: PostWithoutPassword[] = await PostModel
-   .find(filter, projectionPassword)
-   .sort({ _id: -1 })
-   .limit(limit + 1)
-   .lean();
-   
-  const hasNextPage: boolean = posts.length > limit;
-
-  if (hasNextPage) {
-    posts.pop();
-  }
-
-  const optimizedPostList
-    = await Promise.all(posts.map((post: PostWithoutPassword) => optimizePost(post)));
-
-  res.locals.posts = optimizedPostList;
+  res.locals.dbFilter = filter;
   
-  res.locals.hasNextPage = hasNextPage;
-
   next();
 };
 
